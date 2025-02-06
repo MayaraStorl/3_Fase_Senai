bc760d5f43be4a6064ca29d5425fa4eae2dff107
MayaraStorl <mayarastorl@gmail.com>
2025-02-06 17:23:09 -0300

    Fizemos uma pequena revisão do conteudo do ano passado

---
+++ revizona/src/components/Body.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Body.css'
+
+function Body() {
+  return (
+    <div className='container-body'>
+      <img src="" alt="" />
+    </div>
+  )
+}
+
+export default Body
---
+++ revizona/src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+  return (
+    <div className='container-header'>
+        <h4>Logo</h4>
+        <p>Link1</p>
+        <p>Link2</p>
+        <p>Link3</p>
+
+
+    </div>
+  )
+}
+
+export default Header
